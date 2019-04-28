3f9fe7039eaf901816ba05655619f9fa48c052f9
KDVHELL <49861580+KDVHELL@users.noreply.github.com>
2019-04-28 12:58:04 +0500

    Add files via upload

---
+++ HW 28.04/Assets/Components/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float speedcam = 10f;
+
+    float vertical = 0;
+    float horizontal = 0;
+
+    void Update()
+    {
+        vertical = Mathf.Lerp(vertical, Input.GetAxis("Vertical"), Time.deltaTime * 8); //W & S  1 & -1 -> 0    -1..0..1
+        horizontal = Mathf.Lerp(horizontal, Input.GetAxis("Horizontal"), Time.deltaTime * 8);
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        direction = transform.TransformDirection(direction);
+        direction *= speed * Time.deltaTime;
+
+        GetComponent<Rigidbody>().MovePosition(transform.position + direction);
+    }
+}
