d0dd7e5e7a7fae38b578cf99fe1ac50c3acc1c06
KDVHELL <49861580+KDVHELL@users.noreply.github.com>
2019-04-28 19:49:57 +0500

    Add files via upload

---
+++ Codologia class 28.04/Assets/Scripts/Sphere.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float speed = 10f;
+    public float speedcam = 10f;
+    public float jump = 10f;
+
+    float vertical = 0;
+    float horizontal = 0;
+    float up = 0;
+
+    void Update()
+    {
+        vertical = Mathf.Lerp(vertical, Input.GetAxis("Vertical"), Time.deltaTime * 8); //W & S  1 & -1 -> 0    -1..0..1
+        horizontal = Mathf.Lerp(horizontal, Input.GetAxis("Horizontal"), Time.deltaTime * 8);
+        up = Mathf.Lerp(up, Input.GetAxis("Jump"), Time.deltaTime * 8);
+
+        Vector3 direction = new Vector3(horizontal, up, vertical);
+        direction = transform.TransformDirection(direction);
+        direction *= speed * Time.deltaTime;
+
+        GetComponent<Rigidbody>().MovePosition(transform.position + direction);
+    }
+}
---
+++ Codologia class 28.04/Assets/Scripts/movewithcamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movewithcamera : MonoBehaviour
+{
+    private GameObject _sphere;
+    private GameObject _camera;
+
+    public Vector3 Offset { get; set; } = new Vector3;
+
+    void Start()
+    {
+        _sphere = GameObject.Find("Sphere");
+        _camera = GameObject.FindWithTag("MainCamera");
+    }
+
+    void Update()
+    {
+        this.transform.position = _sphere.transform.position
+    }
+}
