9ae43b0d3a9dbf3457ac7b3207859728955655e3
KDVHELL <49861580+KDVHELL@users.noreply.github.com>
2019-05-11 18:33:29 +0500

    Add files via upload

---
+++ Sphere.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float speed = 10f;
+    public float speedcam = 10f;
+    public float jump = 10f;
+
+    private GameObject _feed;
+
+    float vertical = 0;
+    float horizontal = 0;
+    float up = 0;
+
+    void Start()
+    {
+        _feed = GameObject.FindWithTag("Feed");
+    }
+
+    void Update()
+    { 
+
+        if (Input.GetKeyDown("k"))
+        {
+            _feed.gameObject.SetActive(true);
+        }
+        vertical = Mathf.Lerp(vertical, Input.GetAxis("Vertical"), Time.deltaTime * 8); //W & S  1 & -1 -> 0    -1..0..1
+        horizontal = Mathf.Lerp(horizontal, Input.GetAxis("Horizontal"), Time.deltaTime * 8);
+        up = Mathf.Lerp(up, Input.GetAxis("Jump"), Time.deltaTime * 8);
+
+        Vector3 direction = new Vector3(horizontal, up, vertical);
+        direction = transform.TransformDirection(direction);
+        direction *= speed * Time.deltaTime;
+
+        GetComponent<Rigidbody>().MovePosition(transform.position + direction);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+
+        Debug.Log("Минус!");
+
+    }
+}
